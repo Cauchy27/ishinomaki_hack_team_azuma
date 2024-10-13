3ecf10e654bdbd502c526a38a107d2fc03a59c9a
Kohei Matsukawa <k.matsukawa@dual.tokyo>
2024-10-13 06:22:55 +0000

    音声再生のコンポーネント作成

---
+++ app/_component/soundPlay.tsx
@@ -0,0 +1,40 @@
+// 音声の再生
+  // window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  let ctx = new AudioContext();
+  let ctxp = new AudioContext();
+  let EffectSource
+
+   // 音源を取得しAudioBuffer形式に変換して返す関数
+  async function setupEffect(soundUrl:string) {
+    console.log(soundUrl);
+    const response = await fetch(soundUrl);
+
+    console.log(response);
+
+    ctxp = new AudioContext();
+
+    const arrayBuffer = await response.arrayBuffer();
+    // Web Audio APIで使える形式に変換
+    const audioBuffer = await ctxp.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
+
+  function playEffect(ctx:any, audioBuffer:any) {
+    EffectSource = ctx.createBufferSource();
+    // 変換されたバッファーを音源として設定
+    EffectSource.buffer = audioBuffer;
+    // 出力につなげる
+    EffectSource.connect(ctx.destination);
+    EffectSource.start();
+  }
+
+  const soundPlay = async(soundUrl:string) =>{
+
+    const professional = await setupEffect(soundUrl);
+    playEffect(ctxp, professional);
+  }
+
+
+  export {
+    soundPlay
+  }
