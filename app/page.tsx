4641e58d324b73cec2d210f50b5da4936cbf4d9b
Kohei Matsukawa <k.matsukawa@dual.tokyo>
2024-10-13 07:29:39 +0000

    リザルト画面のデモ実装

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,13 @@ import Tittle from "./_screen/title";
 
 const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="">
       {/* <Battle/> */}
-      <Result/>
+      <Result
+        score={70}
+        totalQuestions={10}
+        message="あなたの勝利です！！かわいい❤️"
+      />
       {/* <Select/> */}
       {/* <Tittle/> */}
     </div>
