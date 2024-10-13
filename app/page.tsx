42fce1c77731c0328ba9741e0f5dea3234d144c5
Kohei Matsukawa <k.matsukawa@dual.tokyo>
2024-10-13 05:47:45 +0000

    サウンド追加

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import Result from "./_screen/result";
 import Select from "./_screen/select";
 import Tittle from "./_screen/title";
 
+
+
 const Home = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      ここにとりあえず出すようにして、ステートで表示を切り替えようと思います
-      <Battle/>
+      {/* <Battle/> */}
       <Result/>
-      <Select/>
-      <Tittle/>
+      {/* <Select/>
+      <Tittle/> */}
     </div>
   );
 }
